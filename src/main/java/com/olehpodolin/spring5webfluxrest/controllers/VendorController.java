ed13566f3be2b3328bec2ef5a2adf097906de429
OlegPod <kepacuh@gmail.com>
2019-09-03 22:20:49 +0300

    Implemented POST and PUT for Vendors.Closes #9 #10

--- src/main/java/com/olehpodolin/spring5webfluxrest/controllers/VendorController.java
+++ src/main/java/com/olehpodolin/spring5webfluxrest/controllers/VendorController.java
@@ -2,9 +2,9 @@ package com.olehpodolin.spring5webfluxrest.controllers;
 
 import com.olehpodolin.spring5webfluxrest.domain.Vendor;
 import com.olehpodolin.spring5webfluxrest.repositories.VendorRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -25,5 +25,17 @@ public class VendorController {
     @GetMapping("/api/v1/vendors/{id}")
     public Mono<Vendor> getById(@PathVariable String id) {
         return vendorRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/vendors")
+    public Mono<Void> createVendor(@RequestBody Publisher<Vendor> vendorStream) {
+        return vendorRepository.saveAll(vendorStream).then();
+    }
+
+    @PutMapping("/api/v1/vendors/{id}")
+    public Mono<Vendor> updateVendor(@PathVariable String id, @RequestBody Vendor vendor) {
+        vendor.setId(id);
+        return vendorRepository.save(vendor);
     }
 }
--- src/test/java/com/olehpodolin/spring5webfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/com/olehpodolin/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
 public class VendorControllerTest {
 
@@ -48,5 +50,40 @@ public class VendorControllerTest {
                 .uri("/api/v1/vendors/id")
                 .exchange()
                 .expectBody(Vendor.class);
+    }
+
+    @Test
+    public void createVendor() {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().build()));
+
+        Flux<Vendor> vendorToSave = Flux.just(Vendor.builder().firstName("firstname").lastName("lastname").build());
+
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(vendorToSave, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    public void testUpdateVendor() {
+        //given
+        Mono<Vendor> vendorToUpdate = Mono.just(Vendor.builder().firstName("Vendor").lastName("To Update")
+                .id("someId").build());
+        Mono<Vendor> updatedVendor = Mono.just(Vendor.builder().firstName("Vendor").lastName("Updated")
+                .id("someId").build());
+
+        //when
+        BDDMockito.given(vendorRepository.save(any(Vendor.class)))
+                .willReturn(updatedVendor);
+
+        webTestClient.put()
+                .uri("/api/v1/vendors/someId")
+                .body(vendorToUpdate, Vendor.class)
+                .exchange()
+                .expectBody(Vendor.class)
+                .value(vendor -> vendor.getLastName().equals(updatedVendor.block().getLastName()));
     }
 }
